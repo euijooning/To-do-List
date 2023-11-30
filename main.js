6e70d59156776231ec54f648ec5c86a18a8ca319
EUIJOON <euijoonism@gmail.com>
2023-11-30 13:13:41 +0900

    [feat] 랜덤 아이디 부여하여 구체적으로 어떤 mission인지 구분할 수 있도록 만들기, check 버튼 클릭하면 상태 변화 구현

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
  * Add 버튼을 클릭하면, 할일이 추가된다.
  * Delete 버튼을 누르면 할일이 삭제된다.
  * Check 버튼을 누르면 할일이 끝나면서 밑줄이 쳐진다.
+ * - 1) Check 버튼을 클릭하는 순간 true / false 
+ * - 2) true면 끝난 걸로 간주하고 밑줄 보여주기
+ * - 3) false면 끝나지 않은 걸로 간주하고, 그대로 두기
  * In Progress / Completed 탭을 누르면, 언더바가 이동한다.
  * Completed 탭은 끝난 아이템만, In Progress 탭은 진행 중인 아이템만 나온다.
  * 전체 탭을 누르면 다시 전체 아이템으로 이동
@@ -19,7 +22,9 @@ addButton.addEventListener("click", addMission); // 클릭 이벤트 추가
 
 function addMission() {
     // 각 임무들이 완료되었는지 진행중인지 상태 정보를 알고 있어야 하므로 객체를 활용
+    // 어떤 임무인지 알아야 하므로 id 만들어서 부여
     let mission = {
+        id: generateRandomID(),
         missionDetails: missionInput.value,
         isComplete: false
     };
@@ -35,11 +40,26 @@ function render() {
         resultHTML += `<div class="missions">
         <div>${missionList[i].missionDetails}</div>
         <div>
-            <button>Check</button>
+            <button onclick="toggleChecked('${missionList[i].id}')">Check</button>
             <button>Delete</button>
         </div>
     </div>`
     }
-
     document.getElementById("mission-board").innerHTML = resultHTML;
+}
+
+
+function toggleChecked(id) {
+    // console.log("id: ", id);
+    for(let i = 0; i<missionList.length; i++) {
+        if(missionList[i].id == id) {
+            missionList[i].isComplete = true; // true로 변경
+            break;
+        }
+    }
+    console.log(missionList);
+}
+
+function generateRandomID() {
+    return Math.random().toString(36).substr(2, 12);
 }
